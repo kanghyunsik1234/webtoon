aa4d773e0b81cc0c405de7063dced9b2e10f807c
HyunSik Kang <gamevil1@naver.com>
2018-08-30 07:17:51 +0000

    webtoon ver 1.0.0

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import random
 import requests
 app = Flask(__name__)
+from bs4 import BeautifulSoup
 
 @app.route('/')
 def hello():
@@ -14,7 +15,7 @@ def keyboard():
     #keyboard 딕셔너리 생성
     keyboard = {
         "type" : "buttons",
-        "buttons" : ["ho", "ye", "on"]
+        "buttons" : ["Movie", "Webtoon", "Dog", "Lotto"]
     }
     # json으로 변환
     json_keyboard = json.dumps(keyboard)
@@ -26,21 +27,81 @@ def message():
     # content라는 key의 value를 msg에 저장
     msg = request.json['content']
     img_bool = False
-    if msg == "ho":
-        list2 = ["웹툰1", "웹툰2", "웹툰3", "웹툰4"]
-        return_msg = random.choice(list2)
-    elif msg == "ye":
+    if msg == "Movie":
+        width = 720
+        height = 640
+        img_bool = True
+        url = "https://movie.naver.com/movie/running/current.nhn"
+        req = requests.get(url).text
+        soup = BeautifulSoup(req, 'html.parser')
+        img_tag = soup.select('div.thumb > a > img')
+        
+        title_tag = soup.select('dl.lst_dsc > dt > a')
+        star_tag = soup.select('dl.lst_dsc > dd.star > dl.info_star > dd > div > a > span.num')
+        reserve_tag = soup.select('dl.lst_dsc > dd.star > dl.info_exp > dd > div > span.num')
+        url_tag = soup.select('div.thumb > a > img')
+
+        movie_dic = {}
+        for i in range(0, 10):
+            movie_dic[i] = {
+                'title':title_tag[i].text,
+                'star':star_tag[i].text,
+                'reserve':reserve_tag[i].text,
+                'url':url_tag[i].get('src')
+            }
+        pick_movie = movie_dic[random.randrange(0,10)] 
+        return_msg = "추천영화: %s/평점:%s/예매율:%s :)" % (pick_movie['title'], pick_movie['star'], pick_movie['reserve'])
+        img_url = pick_movie['url']        
+    elif msg == "Lotto":
         # 1~45 리스트
         numbers = list(range(1,46))
         pick = random.sample(numbers, 6)
         return_msg = str(sorted(pick))
     
-    elif msg =="on":
+    elif msg =="Dog":
+        width = 720
+        height = 640
         img_bool = True
         url = "https://api.thedogapi.com/v1/images/search?mime_types=jpg"
         req = requests.get(url).json()
-        dog_url = req[0]['url']
+        return_msg = "오늘의 강아지"
+        img_url = req[0]['url']
+    
+    elif msg =="Webtoon":
+        width = 83
+        height = 90
+        img_bool = True
+        url = "https://comic.naver.com/webtoon/weekday.nhn"
+        req = requests.get(url).text
+        doc = BeautifulSoup(req, 'html.parser')
+        
 
+#comic = doc.select('#content > div.list_area.daily_all > div > div > ul > li > a')[0].text
+
+        title_tag = doc.select('div.col_inner > ul > li > a')
+        img_tag = doc.select('div.thumb > a > img')
+#print(img_tag[0])
+#print(len(title_tag))
+        #list_comics = []
+
+        #for i in title_tag:
+            #list_comics.append(i.text)
+#print(list_comics)
+
+        comic_dic ={}
+
+        for i in range(0,235):
+            comic_dic[i] = {
+                "title": title_tag[i].text,
+                "photo": img_tag[i].get('src'),
+            }
+    
+
+
+        pick_comic = comic_dic[random.randrange(0,235)]
+        return_msg = "추천웹툰: %s :)" % (pick_comic["title"])
+        img_url = pick_comic['photo']
+        
     else:
         return_msg = "웹툰을 클릭하세요 :("
     
@@ -48,16 +109,16 @@ def message():
     if img_bool == True:   
         json_return = {
             "message":{
-                "text": "강아지",
+                "text": return_msg,
                 "photo": {
-                    "url":dog_url,
-                    "width":720,
-                    "height":640
+                    "url":img_url,
+                    "width":width,
+                    "height":height
                 }
             },
             "keyboard": {
                 "type" : "buttons",
-                "buttons" : ["ho", "ye", "on"]
+                "buttons" : ["Movie", "Webtoon", "Dog", "Lotto"]
             }
         }
     else:
@@ -67,7 +128,7 @@ def message():
             },
             "keyboard": {
                 "type" : "buttons",
-                "buttons" : ["ho", "ye", "on"]
+                "buttons" : ["Movie", "Webtoon", "Dog", "Lotto"]
             }
         }
     
--- test.py
+++ test.py
@@ -1,7 +1,32 @@
 import requests
+from bs4 import BeautifulSoup
+import random
 
-url = "https://api.thedogapi.com/v1/images/search?mime_types=jpg"
+url = "https://comic.naver.com/webtoon/weekday.nhn"
+req = requests.get(url).text
+doc = BeautifulSoup(req, 'html.parser')
 
-req = requests.get(url).json()
-print(req[0]['url'])
+#comic = doc.select('#content > div.list_area.daily_all > div > div > ul > li > a')[0].text
 
+title_tag = doc.select('div.col_inner > ul > li > a')
+img_tag = doc.select('div.thumb > a > img')
+#print(img_tag[0])
+#print(len(title_tag))
+list_comics = []
+
+for i in title_tag:
+    list_comics.append(i.text)
+print(len(list_comics))
+
+comic_dic ={}
+
+for i in range(0,10):
+    comic_dic[i] = {
+        "title": title_tag[i].text,
+        "photo": img_tag[i].get('src'),
+    }
+
+
+
+pick_comic = comic_dic[random.randrange(0,10)]
+#print(len(pick_comic))
